765991b356fc2d15f651ba9ac9d576ed6208c2b5
RolinaVorster0101 <rolina@rolinavorster.com>
2026-05-10 22:19:52 +0200

    Simplify connection string configuration for Azure deployment

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TracKeee.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Environment.IsProduction()
-    ? builder.Configuration.GetConnectionString("AzureDbConnection") ?? throw new InvalidOperationException("Connection string 'AzureDbConnection' not found.")
-    : builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
