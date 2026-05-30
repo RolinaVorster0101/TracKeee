c54cba3bbe93ca07956cd46176f37c40d37327c7
RolinaVorster0101 <rolina@rolinavorster.com>
2026-05-30 15:39:04 +0200

    Add Serilog structured logging, CSV exports, and custom error pages

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Serilog;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TracKeee.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+    .WriteTo.Console()
+    .WriteTo.File("Logs/trackeee-.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 30)
+    .CreateLogger();
+builder.Host.UseSerilog();
+Log.Information("=== TracKeee application starting ===");
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
